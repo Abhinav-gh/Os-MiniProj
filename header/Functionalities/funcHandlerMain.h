61af29a6d7e97d4b5083e84165dd18449282e57e
Abhinav Deshpande <abhinav.deshpande@iiitb.ac.in>
2024-05-17 09:41:37 +0530

    Resolve conflict

---
+++ header/Functionalities/funcHandlerMain.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+// includ thing which gives send()
+#include <sys/socket.h>
+
+#ifndef FUNC_HANDLER_MAIN_H
+#define FUNC_HANDLER_MAIN_H
+
+// #define BUFFER_SIZE 4096*4   
+
+#endif
